78063e90d6e80db62e52e89b1861d6f46ce5613a
Maxksim Belousov <bjworld@gmail.com>
2021-09-23 21:22:05 +0300

    upd

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,3 +8,17 @@ mix.copy('src/index.html', 'dist/index.html');
 
 mix.js('src/js/app.js', 'dist/js');
 mix.sass('src/scss/app.scss','dist/css');
+
+mix.webpackConfig({
+  devServer: {
+    hot: true,
+    liveReload: true,
+    // inline: true,
+    // contentBase: __dirname,
+    // disableHostCheck: true,
+    // watchContentBase: true,
+    host: "localhost",
+    open: false,
+    port: 8080,
+  },
+})
