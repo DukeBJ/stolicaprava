7b0a0e080779edda4292917c4a9ba590904b2fef
DukeBJ <bjworld@gmail.com>
2021-09-28 12:43:34 +0300

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,30 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 
 // components can be called from the imported UIkit reference
-UIkit.notification('Hello world.');
+// UIkit.notification('Hello world.');
+
+
+// *********************************************
+// Тут используется JQ, а нужно нативное решение
+// Универсальная функция для проверки расположения объекта в "области видимости"
+// $.fn.onViewPort = function(){
+//   var viewport = {};
+//   viewport.top = $(window).scrollTop();
+//   viewport.bottom = viewport.top + $(window).height();
+//   var bounds = {};
+//   bounds.top = this.offset().top;
+//   bounds.bottom = bounds.top + this.outerHeight();
+//   return ((bounds.top <= viewport.bottom) && (bounds.bottom >= viewport.top));
+// };
+
+// Используем после загрузки DOM
+// $(function() {
+//   // Инициализируем объект, который хотим отслеживать
+//   var $vTarget = $('.view-target');
+//  // Проверяем при скролле страницы
+//   $(window).scroll(function() {
+//     if($vTarget.onViewPort()) {
+//       console.log('Объект в области видимости');
+//     }
+//   });
+// });
